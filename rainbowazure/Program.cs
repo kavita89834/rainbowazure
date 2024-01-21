7629db3dc65243f236c1a82fc02a7e2218084289
kavita89834 <kumarikavita89834@gmail.com>
2024-01-21 21:29:25 +0530

    first commit

---
+++ rainbowazure/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace rainbowazure
+{
+    internal class Program
+    {
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+        public static SqlDataReader reader;
+        public static string constr = "Server=tcp:kavi123.database.windows.net,1433;Initial Catalog=rainbowdb;Persist Security Info=False;User ID=kavi123;Password=Kum@12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+         static void Main(string[] args)
+        {
+            try {
+                con = new SqlConnection(constr);
+                cmd = new SqlCommand("select * from students", con);
+                con.Open();
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("Student ID:\t \t" + reader["StudentID"]);
+                    Console.WriteLine("First Name: \t" + reader["FirstName"]);
+                    Console.WriteLine("Last Name: \t" + reader["LastName"]);
+                    Console.WriteLine("Date Of Birth: \t" + reader["DateOfBirth"]);
+                    Console.WriteLine("Gender: \t" + reader["Gender"]);
+                    Console.WriteLine("Grade Level: \t" + reader["GradeLevel"]);
+                    Console.WriteLine("/n");
+                }
+                    
+            }
+            catch (SqlException ex) { Console.WriteLine("Sever Error" + ex.Message); }
+            catch (Exception ex) { Console.WriteLine("Error!!!" + ex.Message); }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
+
+
